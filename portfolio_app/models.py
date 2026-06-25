8a495c6bb46234630d54aeab4fe51aaffd3e93ea
amirrafiepur <nobody76911@gmail.com>
2026-06-26 03:20:36 +0330

    did some editing and created the projects model
    TODO: creat a fir loop in home.html to show the projects
    
    TODO: create the contact.html and project-single.html

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class projects(models.Model):
+    id = models.AutoField(primary_key=True,unique=True)
+    link = models.CharField(max_length=150)
+    title = models.CharField(max_length=255,blank=True)
+    image = models.ImageField(upload_to='static/uploaded_imgs/')
+    files = models.FileField(upload_to='static/uploaded_files/',null=True,blank=True)
+    Description = models.TextField()
+    status = models.BooleanField(default=False) 
