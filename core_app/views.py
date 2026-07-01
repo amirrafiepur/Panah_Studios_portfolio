07ed460952cf068ca5ca31a3e4424d45eafb15f0
amirrafiepur <nobody76911@gmail.com>
2026-07-01 20:37:01 +0330

    did some beautifications and added a prototype form to contact template

--- core_app/views.py
+++ core_app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from portfolio_app.models import projects
+from contact_app.forms import ContactForm
 
 def home(request):
-    return render(request,'home.html')
+    past_clients=projects.objects.filter(status=True)
+    context={'projects':past_clients}
+    return render(request,'home.html',context)
 
 def contact(request):
-    return render(request,'contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name=request.POST.get('name')
+            subject=request.POST.get('subject')
+            age=request.POST.get('age')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -4,7 +4,7 @@ class projects(models.Model):
     id = models.AutoField(primary_key=True,unique=True)
     link = models.CharField(max_length=150)
     title = models.CharField(max_length=255,blank=True)
-    image = models.ImageField(upload_to='static/uploaded_imgs/')
+    image = models.ImageField(upload_to='uploaded_imgs/')
     files = models.FileField(upload_to='static/uploaded_files/',null=True,blank=True)
     Description = models.TextField()
     status = models.BooleanField(default=False) 
